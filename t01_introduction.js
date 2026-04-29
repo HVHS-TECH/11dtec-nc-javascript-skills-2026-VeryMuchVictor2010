e9a901e0a9ac08075f3a7c2e4ea1a4194f7daa54
VeryMuchVictor2010 <24511vh@hvhs.school.nz>
2026-04-29 02:27:25 +0000

    Apr 29, 2026, 2:27 AM

--- t01_introduction.js
+++ t01_introduction.js
@@ -3,7 +3,7 @@ This is a block comment.
 Put your header comment here!
 ****************************/
 console.log("Running t01_introduction.js")
-console.log(/*Insert your message here!*/)
+console.log("Insert your message here!")
 
 // This is a single line comment
 
--- t02_variables.js
+++ t02_variables.js
@@ -1,1 +1,12 @@
-/* Paste your code from task 1 here */
+/****************************
+This is a block comment.
+Put your header comment here!
+****************************/
+console.log("Running t01_introduction.js")
+console.log("Insert your message here!")
+let username = "victor"
+let year = "2067"
+let age = "15"
+let money = "5 billion dollar"
+// This is a single line comment
+console.log("wsp,"+ username +"as of"+ year +"you got"+ money )
