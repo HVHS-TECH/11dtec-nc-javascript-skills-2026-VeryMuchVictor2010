cfca949081058116634945e62bb440d047955034
VeryMuchVictor2010 <24511vh@hvhs.school.nz>
2026-05-03 22:20:26 +0000

    May 3, 2026, 10:20 PM

--- t03_maths.js
+++ t03_maths.js
@@ -1,1 +1,13 @@
-/* Paste your code from task 2 here */
+/****************************
+This is a block comment.
+Put your header comment here!
+****************************/
+console.log("Running t01_introduction.js")
+console.log("Insert your message here!")
+let username = "victor"
+let year = "2067"
+let age = "15"
+let money = "5 billion dollar"
+// This is a single line comment
+console.log("wsp, "+ username + " as of "+ year +" you got "+ money + " your so rich and famous I wanna be JUST like you when I grow up" )
+console.log ( username +": Ladies ladies, stand back, one at a time!, I know I know, I'm so graet B)")
