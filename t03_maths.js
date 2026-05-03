5826113de65dfa05fd85d3acfd82fdcfc0b60c09
VeryMuchVictor2010 <24511vh@hvhs.school.nz>
2026-05-03 22:36:05 +0000

    May 3, 2026, 10:36 PM

--- t03_maths.js
+++ t03_maths.js
@@ -7,7 +7,8 @@ console.log("Insert your message here!")
 let username = "victor"
 let year = "2067"
 let age = "15"
-let money = "5 billion dollar"
+let money = blaw + 10
+let blaw = 10
 // This is a single line comment
 console.log("wsp, "+ username + " as of "+ year +" you got "+ money + " your so rich and famous I wanna be JUST like you when I grow up" )
 console.log ( username +": Ladies ladies, stand back, one at a time!, I know I know, I'm so graet B)")
