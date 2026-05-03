aac1f6e5826fc5c4afee70185cab49e6a2c3bf83
VeryMuchVictor2010 <24511vh@hvhs.school.nz>
2026-05-03 22:38:20 +0000

    May 3, 2026, 10:38 PM

--- t03_maths.js
+++ t03_maths.js
@@ -7,8 +7,9 @@ console.log("Insert your message here!")
 let username = "victor"
 let year = "2067"
 let age = "15"
-let money = blaw + 10
 let blaw = 10
+let money = 10
+
 // This is a single line comment
-console.log("wsp, "+ username + " as of "+ year +" you got "+ money + " your so rich and famous I wanna be JUST like you when I grow up" )
+console.log("wsp, "+ username + " as of "+ year +" you got "+ money + 10 + " your so rich and famous I wanna be JUST like you when I grow up" )
 console.log ( username +": Ladies ladies, stand back, one at a time!, I know I know, I'm so graet B)")
--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -1,1 +1,15 @@
-/* Paste your code from task 3, Maths here */
+/* Paste your code from task 3, Maths here *//****************************
+This is a block comment.
+Put your header comment here!
+****************************/
+console.log("Running t01_introduction.js")
+console.log("Insert your message here!")
+let username = "victor"
+let year = "2067"
+let age = "15"
+let blaw = 10
+let money = 10
+
+// This is a single line comment
+console.log("wsp, "+ username + " as of "+ year +" you got "+ money + 10 + " your so rich and famous I wanna be JUST like you when I grow up" )
+console.log ( username +": Ladies ladies, stand back, one at a time!, I know I know, I'm so graet B)")
